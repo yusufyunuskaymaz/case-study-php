cd84b0943ecc2ba459d6c29ddbf1ddd2e1504afa
Yusuf Kaymaz <kaymazyusuf88@gmail.com>
2023-04-21 22:10:16 +0300

    search

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const searchInput = document.getElementById('table-search');
 const rows = [...document.getElementsByClassName('table-row')];
 
-
 searchInput.addEventListener('keyup', () => {
     const input = searchInput.value.toLowerCase();
-    rows.forEach((row) => {
-        const task = row.querySelector("#task").textContent.toLowerCase();
-        const title = row.querySelector("#title").textContent.toLowerCase();
-        const description = row.querySelector("#description").textContent.toLowerCase();
-        // const color = row.querySelector("#color").textContent.toLowerCase();
 
-        if (task.includes(input) || title.includes(input) || description.includes(input)) {
-            row.style.display = '';
-        } else {
-            row.style.display = 'none';
+    rows.forEach((row) => {
+        const text = row.textContent.toLowerCase();
+
+        row.style.display = "";
+        if (!row.textContent.toLowerCase().includes(input)) {
+            row.style.display = "none";
         }
     });
+
+    console.log(count);
+
 });
-
-
-
